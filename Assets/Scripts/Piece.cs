762bdb8014ac22312eed1d98d2f9e9defbc9eb52
Martin Horák <horakmartin02@gmail.com>
2024-03-20 00:02:19 +0100

    Pieces added

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Color
+{
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
+
+public class Piece : MonoBehaviour
+{
+
+    [SerializeField] Color _color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
